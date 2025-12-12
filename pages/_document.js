bc69b5278007f2124f314d98eb4eb926a5e7cffa
Luis SIlva <info@lotusmarketingsolutions.com>
2025-12-12 10:19:54 -0500

    favicon

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,12 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="Liquid Suites" />
+        <link rel="manifest" href="/site.webmanifest" />
         <link rel="stylesheet" href="https://use.typekit.net/whl6qkk.css" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
